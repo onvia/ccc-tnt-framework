a83812d6f2b3748f1c917cbe741ddbe526f8e462
onvia <272493431@qq.com>
2023-07-12 11:22:13 +0800

    增加相机震动预览图

--- assets/bundles/camera-controls/scripts/CameraShakeDemo.ts
+++ assets/bundles/camera-controls/scripts/CameraShakeDemo.ts
@@ -9,14 +9,14 @@ export class CameraShakeDemo extends tnt.SceneBase {
     private _shootShake: ShakeParam = null;
     public get shootShake(): ShakeParam {
         if(!this._shootShake){
-            let shakeParam = new ShakeParam();
-            shakeParam.numberOfShakes = 1;
-            shakeParam.shakeAmount = v3(1,0,0);
-            shakeParam.distance = 5;
-            shakeParam.speed = 100;
-            shakeParam.decay = 0.2;
-            shakeParam.randomSign = false;
-            this._shootShake = shakeParam;
+            let shootShake = new ShakeParam();
+            shootShake.numberOfShakes = 1;
+            shootShake.shakeAmount = v3(1,0,0);
+            shootShake.distance = 5;
+            shootShake.speed = 100;
+            shootShake.decay = 0.2;
+            shootShake.randomSign = false;
+            this._shootShake = shootShake;
         }
         return this._shootShake;
     }
@@ -24,14 +24,14 @@ export class CameraShakeDemo extends tnt.SceneBase {
     private _bombShake: ShakeParam = null;
     public get bombShake(): ShakeParam {
         if(!this._shootShake){
-            let shakeParam = new ShakeParam();
-            shakeParam.numberOfShakes = 4;
-            shakeParam.shakeAmount = v3(0.3,1,0);
-            shakeParam.distance = 10;
-            shakeParam.speed = 30;
-            shakeParam.decay = 0.6;
-            shakeParam.randomSign = true;
-            this._bombShake = shakeParam;
+            let bombShake = new ShakeParam();
+            bombShake.numberOfShakes = 4;
+            bombShake.shakeAmount = v3(0.3,1,0);
+            bombShake.distance = 10;
+            bombShake.speed = 30;
+            bombShake.decay = 0.6;
+            bombShake.randomSign = true;
+            this._bombShake = bombShake;
         }
         return this._bombShake;
     }
